79bc46784856e5c8e91bb38b9e4b29864388e688
Kate <katebubnova92@gmail.com>
2023-09-19 14:55:52 +0400

    create new folder sem1

---
+++ Seminars/sem1/Program.cs
@@ -0,0 +1,22 @@
+﻿//Console.WriteLine("Input number: "); //
+//int num = Convert.ToInt32(Console.ReadLine());//создаем переменную, которая хранит целые числа используя конвертатор строки в число
+//Console.WriteLine($"Your number -> {num} ");
+
+//Задача 1. Напишите программу, которая на вход принимает число и выдает его квадрат
+
+//Console.WriteLine("Input number: ");
+//int num = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine($"Quad of {num} -> {num*num}");
+
+//Задача 2. Напишите программу, которая принимает на вход трехзначное число и на выходе показывает последнюю цифру этого числа
+//Console.WriteLine("Input three-digit number: ");
+//int num = Convert.ToInt32(Console.ReadLine());
+//if(num >= 100 && num < 1000)
+//{
+//    Console.WriteLine($"Last digit of {num} -> {num%10}");
+//}
+//else 
+//{
+//    Console.WriteLine("you input not three-digit number");
+//}
+
