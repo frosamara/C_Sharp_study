749fb9457a9352ad008cf459b5e7aee8eb8d10e2
Kate <katebubnova92@gmail.com>
2023-09-20 15:06:44 +0400

    add code vs using massiv

--- lection02/lesson01/Program.cs
+++ lection02/lesson01/Program.cs
@@ -1,4 +1,6 @@
-﻿// Задача. Найти максимум из 9 чисел
+﻿// Работа с функциями
+
+// Задача. Найти максимум из 9 чисел
 // первый способ без использования функций
 // int a1 = 15;
 // int b1 = 21;
@@ -23,25 +25,25 @@
 // if(c3 > max) max = c3;
 
 // Console.WriteLine(max);
-
+//-------------------------------------------
 //второй вариант с использованием функции
-int max(int arg1, int arg2, int arg3)
-{
-    int result = arg1;
-    if (arg2 > result) result = arg2;
-    if (arg3 > result) result = arg3;
-    return result;
-}
+// int max(int arg1, int arg2, int arg3)
+// {
+//     int result = arg1;
+//     if (arg2 > result) result = arg2;
+//     if (arg3 > result) result = arg3;
+//     return result;
+// }
 
-int a1 = 15;
-int b1 = 21;
-int c1 = 39;
-int a2 = 12;
-int b2 = 23;
-int c2 = 33;
-int a3 = 13;
-int b3 = 2003;
-int c3 = 303;
+// int a1 = 15;
+// int b1 = 21;
+// int c1 = 39;
+// int a2 = 12;
+// int b2 = 23;
+// int c2 = 33;
+// int a3 = 13;
+// int b3 = 2003;
+// int c3 = 303;
 
 // 1ый вариант вывода функции
 // int max1 = max(a1, b1, c1);
@@ -50,5 +52,23 @@ int c3 = 303;
 // int res = max(max1, max2, max3);
 
 // 2ой вариант вывода функции
-int res = max(max(a1, b1, c1), max(a2, b2, c2), max(a3, b3, c3));
+// int res = max(max(a1, b1, c1), max(a2, b2, c2), max(a3, b3, c3));
+// Console.WriteLine(res);
+//-----------------------------------------
+
+// Работа с массивами
+int max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if (arg2 > result) result = arg2;
+    if (arg3 > result) result = arg3;
+    return result;
+}
+//              0   1   2   3   4   5   6   7   8
+int [] array = {15, 21, 39, 12, 23, 33, 13, 24, 35};
+int res = max(
+    max(array[0], array[1], array[2]),
+    max(array[3], array[4], array[5]),
+    max(array[6], array[7], array[8])
+);  
 Console.WriteLine(res);
