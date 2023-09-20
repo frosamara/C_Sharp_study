48ccc1d3f5b1ed78dc0942384e1448c9f1897a52
Kate <katebubnova92@gmail.com>
2023-09-20 20:31:45 +0400

    вторая задача с массивом

--- lection02/lesson01/Program.cs
+++ lection02/lesson01/Program.cs
@@ -57,18 +57,36 @@
 //-----------------------------------------
 
 // Работа с массивами
-int max(int arg1, int arg2, int arg3)
-{
-    int result = arg1;
-    if (arg2 > result) result = arg2;
-    if (arg3 > result) result = arg3;
-    return result;
-}
-//              0   1   2   3   4   5   6   7   8
-int [] array = {15, 21, 39, 12, 23, 33, 13, 24, 35};
-int res = max(
-    max(array[0], array[1], array[2]),
-    max(array[3], array[4], array[5]),
-    max(array[6], array[7], array[8])
-);  
-Console.WriteLine(res);
+// int max(int arg1, int arg2, int arg3)
+// {
+//     int result = arg1;
+//     if (arg2 > result) result = arg2;
+//     if (arg3 > result) result = arg3;
+//     return result;
+// }
+// //              0   1   2   3   4   5   6   7   8
+// int [] array = {15, 21, 39, 12, 23, 33, 13, 24, 35};
+// int res = max(
+//     max(array[0], array[1], array[2]),
+//     max(array[3], array[4], array[5]),
+//     max(array[6], array[7], array[8])
+// );  
+// Console.WriteLine(res);
+//---------------------------------------------------
+
+int [] array = {1,2,3,4,5,6,7,8};
+
+int n = array.Length; //определение длины массива
+int find = 4;
+int index = 0;
+
+while (index < n)
+    {
+        if(array[index] == find)
+        {
+            Console.WriteLine(index);
+            break;
+        }
+        index++; // index = index + 1
+    }
+
