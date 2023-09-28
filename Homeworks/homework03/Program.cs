314e4cfcdb3e4f908f725adb2750182206dc2acf
Kate <katebubnova92@gmail.com>
2023-09-29 00:00:03 +0400

    doing homework3

---
+++ Homeworks/homework03/Program.cs
@@ -0,0 +1,131 @@
+﻿//Задача 1. Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом
+//Метод должен проверить является ли число пятизначным, в противном случае - вывести Число не пятизначное и False в следующей строке.
+//Для остальных чисел вернуть True или False.
+
+// using System;
+// public class Answer
+// {
+//     static bool IsPalindrome(int number){
+//         if (number < 10000 || number >= 100000){
+//             Console.WriteLine("Число не пятизначное");
+//             return false;
+//         }
+
+//         int reverse = 0;
+//         int temp = number;
+//         while (temp > 0){
+//             reverse = reverse * 10 + temp % 10;
+//             temp /= 10;
+//         }
+//         return reverse == number;
+//     }
+
+//       static public void Main(string[] args) {
+//         int number;
+//         if (args.Length >= 1) {
+//             number = int.Parse(args[0]);
+//         } 
+//         System.Console.WriteLine("Input number: ");
+//        int n = Convert.ToInt32(Console.ReadLine());
+//         bool result = IsPalindrome(n);
+//         System.Console.WriteLine($"{result}");
+//     }
+// }
+
+//Задача 2. Внутри класса Answer напишите метод ShowCube, который принимает на вход число (N) и выводит таблицу кубов чисел от 1 до N (включительно) каждое на новой строке.
+//N = 3 
+/*
+1
+8
+27
+*/
+//N = 5
+/*
+1
+8
+27
+64
+125
+*/
+
+using System;
+public class Answer
+{
+    static void ShowCube(int N)
+    {
+        for (int i = 1; i <= N; i++)
+        {
+            Console.WriteLine(Cube(i));
+        }
+    }
+
+    public static int Cube(int number)
+    {
+        return number * number * number;
+    }
+
+      static public void Main(string[] args) {
+        int N;
+
+        if (args.Length >= 1) {
+            N = int.Parse(args[0]);
+        } else {
+            N = 5;
+        }
+
+        ShowCube(N);
+    }
+}
+
+
+//Задача 3. Внутри класса Answer напишите метод DistanceBetweenPoints, который принимает на вход координаты двух точек pointA и pointB в виде массива целых чисел, и возвращает расстояние между ними в 3D пространстве.
+// A (3,6,8); B (2,1,-7) -> 15.84
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+// using System;
+// public class Answer
+// {
+//     private static int Square(int number)
+//     {
+//         return number * number;
+//     }
+
+//     private static int SumSquare(int[] pointA, int[] pointB)
+//     {
+//         int sum = 0;
+//         for (int i = 0; i < pointA.Length; i++)
+//         {
+//             sum += Square(pointA[i] - pointB[i]);
+//         }
+//         return sum;
+//     }
+
+//     private static double Length(int[] pointA, int[] pointB)
+//     {
+//         double result = Math.Sqrt(SumSquare(pointA, pointB));
+//         return result;
+//     }
+
+//     public static void Main(string[] args) {
+//         int x1, x2, x3, y1, y2, y3;
+
+//         if (args.Length >= 6) {
+//             x1 = int.Parse(args[0]);
+//             x2 = int.Parse(args[1]);
+//             x3 = int.Parse(args[2]);
+//             y1 = int.Parse(args[3]);
+//             y2 = int.Parse(args[4]);
+//             y3 = int.Parse(args[5]);
+//         } else {
+//             x1 = 7;
+//             x2 = -5;
+//             x3 = 0;
+//             y1 = 1;
+//             y2 = -1;
+//             y3 = 9;
+//         }
+
+//         double result = Length(new int[]{x1, x2, x3}, new int[]{y1, y2, y3});
+//         Console.WriteLine($"{result:F2}");
+//     }
+// }
